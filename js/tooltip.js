1c2a571c0ec6d948f3b48ffa23f3a0e13b44e0ad
Veljko Jovicic <54376393+CymkWins@users.noreply.github.com>
2020-03-18 22:06:44 +0000

    Add files via upload

---
+++ js/tooltip.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('[data-toggle="twitter"]').tooltip();
+    $('[data-toggle="github"]').tooltip();
+    $('[data-toggle="mail"]').tooltip();
+    $('[data-toggle="instagram"]').tooltip();
+});
